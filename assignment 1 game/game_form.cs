7395444caf15c31a9ec0a2c2bbc42f36ae9b3a7b
rohin panchal <rohinpanchal@gmail.com>
2020-11-09 15:26:00 +1300

    done all comments

--- assignment 1 game/game_form.cs
+++ assignment 1 game/game_form.cs
@@ -19,42 +19,42 @@ namespace assignment_1_game
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            btn_spin.Enabled = false;
-            btn_shoot.Enabled = false;
-            btn_shootaway.Enabled = false;
+            btn_spin.Enabled = false;//disabling the spin button//
+            btn_shoot.Enabled = false;//disabling the shoot button//
+            btn_shootaway.Enabled = false;//disbaling the shootaway button//
         }
 
         private void btn_load_Click(object sender, EventArgs e)
         {
-            btn_spin.Enabled = true;
-            btn_load.Enabled = false;
+            btn_spin.Enabled = true;//enabling the spin button//
+            btn_load.Enabled = false;//disabling the load button//
             
         }
 
         private void btn_spin_Click(object sender, EventArgs e)
         {
-            btn_spin.Enabled = false;
-            btn_shoot.Enabled = true;
+            btn_spin.Enabled = false;//disabling the spin button//
+            btn_shoot.Enabled = true;//enabling the shoot button//
         }
 
         private void btn_shoot_Click(object sender, EventArgs e)
         {
-            btn_shootaway.Enabled = true;
-            btn_shoot.Enabled = false;
+            btn_shootaway.Enabled = true;//enabling the shootaway button//
+            btn_shoot.Enabled = false;//disabling the shoot button//
         }
 
         private void btn_playagain_Click(object sender, EventArgs e)
         {
-            btn_load.Enabled = true;
-            btn_spin.Enabled = false;
-            btn_shoot.Enabled = false;
-            btn_shootaway.Enabled = false;
+            btn_load.Enabled = true;//enabling the load button//
+            btn_spin.Enabled = false;//disabling the spin button//
+            btn_shoot.Enabled = false;//disabling the shoot button//
+            btn_shootaway.Enabled = false;//disabling the shootaway button//
         }
 
         private void btn_shootaway_Click(object sender, EventArgs e)
         {
-            btn_shoot.Enabled = false;
-            btn_shootaway.Enabled = false;
+            btn_shoot.Enabled = false;//disabling the shoot button//
+            btn_shootaway.Enabled = false;//disabling the shootaway button//
         }
     }
 }
